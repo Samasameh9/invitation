c0b75cdf134ebf61d28ca76aa0d0589d8ada1040
Samasameh9 <samaasamehh24@gmail.com>
2026-06-30 20:04:45 +0300

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
  const button = document.getElementById("clickedButton");
-  const envelope = document.querySelector(".relative");
-
+  const envelope = document.getElementById("envelope");
+ const main = document.getElementById("mainContent");
+ const bottomFlap = document.getElementById("bottomFlap");
   button.addEventListener("click", () => {
     envelope.classList.toggle("open");
   });
+  bottomFlap.addEventListener("transitionend", () => {
+  envelope.classList.add("hidden");
+  main.classList.remove("hidden");
+  main.classList.add("block");
+}, { once: true });
